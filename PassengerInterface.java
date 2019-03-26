caa8787778456538a8f5f30a513f4bcd4e75f415
huang <huang_guibo@outlook.com>
2019-03-26 21:00:26 +0800

    第一次上传

---
+++ PassengerInterface.java
@@ -0,0 +1,12 @@
+package com.example.lib.Passenger;
+
+public interface PassengerInterface {
+
+	public String getName();
+
+	public int getBookingNumber();
+
+	public int getRow();
+
+	public int getSeatPosition();
+}
