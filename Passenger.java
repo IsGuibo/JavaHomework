09022fc1d2f2f9b71faf2fd05f8909049e3cc657
huang <huang_guibo@outlook.com>
2019-03-26 21:08:47 +0800

    第二次提交

---
+++ Passenger.java
@@ -0,0 +1,26 @@
+package com.example.lib.Passenger;
+
+public class Passenger implements PassengerInterface {
+	private String names;
+	private int bookingNumber;
+	private int rows; 							
+	private int seatPosition;
+	public Passenger(String names, int bookingNumber, int rows, int seatPosition) {
+		this.names = names;
+		this.bookingNumber = bookingNumber;
+		this.rows = rows;
+		this.seatPosition = seatPosition;
+	}
+	public String getName() {
+		return names;
+	}
+	public int getBookingNumber() {
+		return bookingNumber;
+	}
+	public int getRow() {
+		return rows;
+	}
+	public int getSeatPosition() {
+		return seatPosition;
+	}
+}
