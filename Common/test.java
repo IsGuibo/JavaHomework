d3bf6981446cfef00110e6de4b38d5f82b0eee91
huang <huang_guibo@outlook.com>
2019-04-09 23:57:25 +0800

    将本字符串转换成一个字符串数组，要求每个数组元素都是一个有意义的英文单词,并输出到控制台

--- Common/test.java
+++ Common/test.java
@@ -16,6 +16,11 @@ public class test {
         }
         System.out.println(sb.toString());
 
+        //将本字符串转换成一个字符串数组，要求每个数组元素都是一个有意义的英文单词,并输出到控制台
+        String[] b = str.split("\\s+");
+        for(String s:b){
+            System.out.print(s+"|");
+        }
     }
 
     //统计该字符串中字母s出现的次数
