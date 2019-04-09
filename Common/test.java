634d2697518335060d076cc41c3a52c4691b45bc
huang <huang_guibo@outlook.com>
2019-04-09 23:55:11 +0800

    用两种方式实现该字符串的倒序输出。(用StringBuffer和for循环方式分别实现)

--- Common/test.java
+++ Common/test.java
@@ -3,27 +3,38 @@ package com.example.lib.JavaHomework.Common;
 
 public class test {
     public static void main(String[] args) {
-        final   String str = "this is a test of java";
+        final String str = "this is a test of java";
         System.out.println(appendCount(str));
-        System.out.println(str.substring(10,14));//取出子字符串"test"
+        System.out.println(str.substring(10, 14));//取出子字符串"test"
         InitialCapitalization(str);
-    }
-    //统计该字符串中字母s出现的次数
-    private static int appendCount(String str){
-        int count =0;
-        char[] chars=str.toCharArray();//将本字符串复制到一个字符数组Char[] str中.// toCharArray
+// 用两种方式实现该字符串的倒序输出。(用StringBuffer和for循环方式分别实现)
+        StringBuffer sb = new StringBuffer(str);
+        System.out.println(sb.reverse());
+        StringBuffer stringBuffer = new StringBuffer();
+        for(int i=str.length()-1;i>=0;i--){
+            stringBuffer.append(str.charAt(i));
+        }
+        System.out.println(sb.toString());
 
-        for(char c:chars){
-            if(c==115){
+    }
+
+    //统计该字符串中字母s出现的次数
+    private static int appendCount(String str) {
+        int count = 0;
+        char[] chars = str.toCharArray();//将本字符串复制到一个字符数组Char[] str中.// toCharArray
+
+        for (char c : chars) {
+            if (c == 115) {
                 count++;
             }
         }
         return count;
     }
+
     //将字符串中每个单词的第一个字母变成大写， 输出到控制台。//用正则表达式
-    private static  void  InitialCapitalization(String str){
-        String[] b =str.split("\\s+");
-        StringBuffer sb =new StringBuffer();
+    private static void InitialCapitalization(String str) {
+        String[] b = str.split("\\s+");
+        StringBuffer sb = new StringBuffer();
         for (String aB : b) {
             sb.append(String.valueOf(aB.charAt(0)).toUpperCase()).append(aB.substring(1)).append(" ");
         }
