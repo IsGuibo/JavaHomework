9b8cea704272da4d69196b15b0b1dfb108533319
huang <huang_guibo@outlook.com>
2019-04-10 18:18:13 +0800

    包装类与基本数据类型之间转换,其他类型操作相似。

---
+++ Common/test_3.java
@@ -0,0 +1,10 @@
+package com.example.lib.JavaHomework.Common;
+
+public class test_3 {
+//写一个例子，实现包装类与基本数据类型之间转换；实现包装类与字符串相互转换；实现基本数据类型与字符串之间相互转换
+    public static void main(String[] args) {
+        int num = 1;
+        Integer integer = new Integer(num);
+        num = integer.intValue();//包装类与基本数据类型之间转换
+    }
+}
