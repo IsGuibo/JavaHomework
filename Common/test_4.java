c5f9a90a949f6f566bb4f15bf20ff8249d81d9c5
huang <huang_guibo@outlook.com>
2019-04-10 19:06:05 +0800

    将一个字符串中所有的ip地址打印出来

--- Common/test_4.java
+++ Common/test_4.java
@@ -10,7 +10,7 @@ public class test_4 {
         String regExp = "\\(\\d{4}\\)\\d{4}-\\d{4}";
         Pattern p = Pattern.compile(regExp);
         Matcher m = p.matcher(str);
-        //将一个字符串中所有的ip地址打印出来
+        // 将一个字符串中所有的ip地址打印出来
         String parentString="HuangGuibo_IP:127.168.220.221";
         String regexString=".*(\\d{3}(\\.\\d{1,3}){3}).*";
         String IPString=parentString.replaceAll(regexString,"$1");
