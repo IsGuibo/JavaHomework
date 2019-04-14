2bf3b11d577b131f6ee16cf0440aefc69e0f39b6
huang <huang_guibo@outlook.com>
2019-04-14 19:11:19 +0800

    //将一个字符串中的所有整数提取出来，保存在一维数组中。比如”ae256dd—w348e6”，提取出256,348,6三个整数

--- Common/test_4.java
+++ Common/test_4.java
@@ -9,22 +9,27 @@ public class test_4 {
         String str = "(0000)0000-0000";
         String regExp = "\\(\\d{4}\\)\\d{4}-\\d{4}";
         Pattern p = Pattern.compile(regExp);
-        Matcher m = p.matcher(str);
-        System.out.println(m.matches());
+        System.out.println(p.matcher(str).matches());
         // 将一个字符串中所有的ip地址打印出来
         String parentString="HuangGuibo_IP:127.168.220.221q";
         String regexString=".*(\\d{3}(\\.\\d{1,3}){3}).*";
-        String IPString=parentString.replaceAll(regexString,"$1");
-        System.out.println(IPString);
+        System.out.println(parentString.replaceAll(regexString,"$1"));
         //将字符串中连续出现的字符压缩成一个。比如aaabbbccccd，压缩后变成abcd。
-        String s = "aaabbbccccd";
-        s = s.replaceAll("([a-z])\\1+", "$1");
-        System.out.println(s);
+        str = "aaabbbccccd";
+        System.out.println(str.replaceAll("([a-z])\\1+", "$1"));
         //输入一个字符串，判断该字符串是否以abc结尾
-        String regExp_2 = ".*abc$";
-        Pattern p_2 = Pattern.compile(regExp_2);
-        Matcher m_2 = p_2.matcher(str);
-        System.out.println(m_2.matches());
+        regExp = ".*abc$";
+        p = Pattern.compile(regExp);
+        System.out.println(p.matcher(str).matches());
+        //将一个字符串中的所有整数提取出来，保存在一维数组中。比如”ae256dd—w348e6”，提取出256,348,6三个整数
+        str = "ae256dd—w348e6";
+        System.out.println( str.replaceAll("(\\D+)","|"));
+        //文本替换：将字符串中所有的”pupel”替换为”pupil”。
+        str = "pupel";
+        System.out.println(  str.replaceAll("pupel","pupil"));
+
+
+
 
 
     }
