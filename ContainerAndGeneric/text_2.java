7f68b860190dcba8d3e79c84f4f9b33994f8b34e
huang <huang_guibo@outlook.com>
2019-05-06 21:06:06 +0800

    每一个学生都有对应的归属地。??学生Student，地址String。??学生属性：姓名，年龄。

---
+++ ContainerAndGeneric/text_2.java
@@ -0,0 +1,32 @@
+package com.example.lib.JavaHomework.ContainerAndGeneric;
+
+import java.util.Map;
+
+public class text_2 {
+    public static void main(String[] args) {
+
+    }
+    private class Student{
+        String name;
+        int age;
+        public int getAge() {
+            return age;
+        }
+
+        public void setAge(int age) {
+            this.age = age;
+        }
+
+
+
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+    }
+}
+
